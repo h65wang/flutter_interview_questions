f5879651a8a8adbc59831be65ceddfa9a0399193
bradley <h65wang@uwaterloo.ca>
2023-06-28 09:07:37 -0700

    Add [Question], [Repo] and [RepoProvider]

---
+++ lib/model/question.dart
@@ -0,0 +1,31 @@
+class Question {
+  final String title;
+  final String answer;
+  final List<String> candidates;
+  final List<String> tags;
+  final int? difficulty;
+  final String? credit;
+
+  Question.fromJson(json)
+      : title = json['title'] ?? json['question'],
+        answer = json['answer'],
+        candidates = List.from(json['candidates']),
+        tags = List.from(json['tags']),
+        difficulty = json['difficulty'],
+        credit = json['credit'];
+
+  Map<String, dynamic> toJson() {
+    return {
+      'title': title,
+      'answer': answer,
+      'candidates': candidates,
+      'tags': tags,
+      'difficulty': difficulty,
+      'credit': credit,
+    };
+  }
+
+  @override
+  String toString() =>
+      'Q(${title.length > 20 ? '${title.substring(0, 17)}...' : title})';
+}
