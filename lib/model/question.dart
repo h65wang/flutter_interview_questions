37688152ece753d1249bf09df1366b891691efaa
huanghui <1224991097@qq.com>
2023-07-07 18:04:47 +0800

    遵循lint规范

--- lib/model/question.dart
+++ lib/model/question.dart
@@ -7,15 +7,15 @@ class Question {
   final int? difficulty;
   final String? credit;
 
-  Question.fromJson(json)
-      : title = json['title'] ?? json['question'],
-        answer = json['answer'],
-        candidates = List.from(json['candidates']),
-        tags = List.from(json['tags']),
-        difficulty = json['difficulty'],
-        credit = json['credit'];
+  Question.fromJson(Map<String, dynamic> json)
+      : title = (json['title'] ?? json['question']) as String,
+        answer = (json['answer']) as String,
+        candidates = List.from(json['candidates'] as List),
+        tags = List.from(json['tags'] as List),
+        difficulty = json['difficulty'] as int?,
+        credit = json['credit'] as String?;
 
-  Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => <String, dynamic>{
         'title': title,
         'answer': answer,
         'candidates': candidates,
