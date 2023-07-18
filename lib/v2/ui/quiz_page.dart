beda2500cfd1e5031f475a926ec1ba2d6aa81525
Jefferson <archkernel@outlook.com>
2023-07-18 22:57:28 +0800

    :art: lint code

--- lib/v2/ui/quiz_page.dart
+++ lib/v2/ui/quiz_page.dart
@@ -189,6 +189,7 @@ class _QuizItemCardState extends State<_QuizItemCard> {
             q.choices.forEach((choice) {
               choice.selected = false;
             });
+
             /// Let's do this first,
             /// the status here should be changed
             /// when the user clicks on the submit button.
